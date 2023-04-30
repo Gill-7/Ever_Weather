08e9d85f084a2d150f7fd52195980c44942d2138
Gill <gillpreetsinghgill@gmail.com>
2023-04-29 19:18:32 -0700

    fixed error with api call

--- src/js/apiFunction.js
+++ src/js/apiFunction.js
@@ -21,7 +21,7 @@ function getCityCoords(cityName) {
 }
 
 function getWeatherDataByCoords(lat, lon) {
-  return `http://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&units=metric&exclude=minutely,alerts&appid=d49c28c23cd27065aa0c784c5bf5d216`;
+  return `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&units=metric&exclude=minutely,alerts&appid=d49c28c23cd27065aa0c784c5bf5d216`;
 }
 
 export { formData, getCityName, getCityCoords, getWeatherDataByCoords };
